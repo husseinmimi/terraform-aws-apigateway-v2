e99570cd62418f1118a9afb544ce0808e0ce5c3b
husseinmimi <85610131+husseinmimi@users.noreply.github.com>
2023-07-26 11:29:58 +0300

    tls_config

--- variables.tf
+++ variables.tf
@@ -232,7 +232,7 @@ variable "integrations" {
     content_handling_strategy = optional(string, null)
     credentials_arn = optional(string, null)
     request_parameters = optional(string, null)
-    tls_config = optional(any,null)
+    tls_config = optional(object({server_name_to_verify =  string}),null)
     response_parameters = optional(any,null)
   }))
   default     = {}
