5bd8c4d35bcd286ca1ac99071b210a80b8a8db0c
husseinmimi <85610131+husseinmimi@users.noreply.github.com>
2023-07-30 13:13:59 +0300

    fix

--- main.tf
+++ main.tf
@@ -178,7 +178,7 @@ resource "aws_apigatewayv2_integration" "this" {
     for_each = each.value["tls_config"] != {} ? {tls_config = each.value["tls_config"]} : {}
 
     content {
-      server_name_to_verify = each.value.tls_config["server_name_to_verify"] # TODO: check later
+      server_name_to_verify = tls_config["server_name_to_verify"] # TODO: check later
     }
   }
 
